a98a9a28dab0e8eed4aba4a70b4496f306ef32e0
cperceful <cpercef1@gmail.com>
2017-11-16 19:42:19 -0600

    LoginActivity built, created files for Register and Main activities

--- app/src/main/java/com/cperceful/beercellartracker/LoginActivity.java
+++ app/src/main/java/com/cperceful/beercellartracker/LoginActivity.java
@@ -1,13 +1,108 @@
 package com.cperceful.beercellartracker;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView emailView;
+    private EditText passwordView;
+    private Button signInButton;
+    private Button registerButton;
+    private FirebaseAuth firebaseAuth;
+
+    private static final String DEBUG_TAG = "BeerCellarTracker";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailView = findViewById(R.id.autoCompleteText_email);
+        passwordView = findViewById(R.id.editText_password);
+        signInButton = findViewById(R.id.button_signIn);
+        registerButton = findViewById(R.id.button_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        passwordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == R.id.login || actionId == EditorInfo.IME_NULL){
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void attemptLogin(){
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()){
+            return;
+        } else {
+            Toast.makeText(this, "The water's fine my guy, jumping in", Toast.LENGTH_SHORT);
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(DEBUG_TAG, "Login successful: " + task.isSuccessful());
+
+                if (!task.isSuccessful()){
+                    Log.d(DEBUG_TAG, "Problem signing in: " + task.getException());
+                    generateErrorMessage("oof ouch owie sign in failed");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void generateErrorMessage(String message){
+        new AlertDialog.Builder(this)
+                .setTitle("RED ALERT")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
